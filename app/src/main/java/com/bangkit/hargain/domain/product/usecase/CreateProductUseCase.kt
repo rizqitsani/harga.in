74cef3bf90dde081aeedf083eefa496f692fb0ea
rizqitsani <rizqitsani@gmail.com>
2022-06-07 18:52:21 +0700

    feat: add delete product

--- app/src/main/java/com/bangkit/hargain/domain/product/usecase/CreateProductUseCase.kt
+++ app/src/main/java/com/bangkit/hargain/domain/product/usecase/CreateProductUseCase.kt
@@ -9,8 +9,18 @@ import com.bangkit.hargain.presentation.common.base.BaseResult
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+<<<<<<<< HEAD:app/src/main/java/com/bangkit/hargain/domain/product/usecase/CreateProductUseCase.kt
 class CreateProductUseCase @Inject constructor(private val productRepository: ProductRepository) {
     suspend fun invoke(productCreateRequest: ProductCreateRequest) : Flow<BaseResult<ProductEntity, WrappedResponse<ProductResponse>>> {
         return productRepository.createProduct(productCreateRequest)
+========
+class ProductUseCase @Inject constructor(private val productRepository: ProductRepository) {
+    suspend fun getDetail(productId: String): Flow<BaseResult<ProductEntity, WrappedResponse<ProductResponse>>> {
+        return productRepository.getProductDetail(productId)
+>>>>>>>> ab511b7 (feat: add delete product):app/src/main/java/com/bangkit/hargain/domain/product/usecase/ProductUseCase.kt
+    }
+
+    suspend fun delete(productId: String): Flow<BaseResult<ProductEntity, WrappedResponse<ProductResponse>>> {
+        return productRepository.deleteProduct(productId)
     }
 }
